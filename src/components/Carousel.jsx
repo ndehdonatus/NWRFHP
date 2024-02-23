47a6b1fc374cb213e1885c8e5f8ddb02bfa7115b
ndehdonatus <ndehdonatus@gmail.com>
2024-02-23 08:17:53 +0100

    I have added more images in slider

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -19,12 +19,109 @@ export default function DefaultCarousel() {
                 src="/images/096A0599.png"
             />
             <Image className='h-full'
-                alt="slider image"  width={1200} height={1200}
+                alt="slider image" width={1200} height={1200}
                 src="/images/rock.jpg"
             />
             <Image className='h-full'
                 alt="slider image" width={1200} height={1200}
                 src="/images/head-office.jpg"
+            />
+
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/nnfca.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/vanca.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/nkca.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/substore.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/csmu1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/fonmedal1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1500} height={1500}
+                src="/images/sam and maggi.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/fondoh1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/fondohmedal1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/yembe1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/ndzi1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/comca.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1300}
+                src="/images/fonmaggi.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/odrana1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/fomuso1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/mbakong1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/management1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/fonca.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/ndehca.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/intenship.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/fon3.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/medalists.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/maggi1.jpg"
+            />
+            <Image className='h-full'
+                alt="slider image" width={1200} height={1200}
+                src="/images/medalists2.jpg"
             />
         </Carousel>
     )
